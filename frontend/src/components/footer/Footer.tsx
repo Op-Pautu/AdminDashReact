ed94271e5b5a7a5e77acdf0a27f435fe1d88fc78
Op-Pautu <pautuop@gmail.com>
2023-07-18 13:00:01 +0530

    Fetch users data from backend

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './footer.scss'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <span>opadmin</span>
+      <span>© Op Admin Dashboard</span>
+    </div>
+  )
+}
